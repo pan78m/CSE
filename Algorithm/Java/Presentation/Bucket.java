d422c2b1c2a2d7eb3034da2a26c3964160d5ce2a
pan78m <aryanpankaj78@gmail.com>
2023-06-21 22:53:23 +0600

    bu

--- Algorithm/Java/Presentation/Bucket.java
+++ Algorithm/Java/Presentation/Bucket.java
@@ -32,7 +32,7 @@ public class Bucket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int[] A = new int[N]-.0ii                                     i9=
+        int[] A = new int[N];             
         for (int i = 0; i < N; i++) {
             A[i] = scanner.nextInt();
         }
